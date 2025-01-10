1be8172cf0b916acce6ac3ae7b6e364732afb01c
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2025-01-10 14:48:33 +0100

    responsive my projects

--- src/Pages/MyProjects.js
+++ src/Pages/MyProjects.js
@@ -5,18 +5,18 @@ export default function MyProjects() {
     return (
         <div className="part2">
             <div className="menubar">
-                    <div className="menubar1">
-                        <NavLink className="menubar-btn1" to="/">get to know me</NavLink>
-                    </div>
-                    <div className="menubar2">
-                        <NavLink className="menubar-btn" to="/">HOME</NavLink>
-                        <NavLink className="menubar-btn" to="/aboutme">ABOUT ME</NavLink>
-                    </div>
+                <div className="menubar1">
+                    <NavLink className="menubar-btn1" to="/">get to know me</NavLink>
                 </div>
-                <h2 class="animate__animated animate__fadeInRight">My projects</h2>
-                <div className="projects">
-                    <a href="https://skisnjesko.com/" target="_blanc"><img src="./Pictures/ski.jpg" alt="ski site" /></a>
+                <div className="menubar2">
+                    <NavLink className="menubar-btn" to="/">HOME</NavLink>
+                    <NavLink className="menubar-btn" to="/aboutme">ABOUT ME</NavLink>
                 </div>
+            </div>
+            <h2 class="animate__animated animate__fadeInRight">My projects</h2>
+            <div className="projects">
+                <a href="https://skisnjesko.com/" target="_blanc"><img src="./Pictures/ski.jpg" alt="ski site" /></a>
+            </div>
         </div>
     )
 }
